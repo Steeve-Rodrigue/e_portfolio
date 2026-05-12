27db65b94161040344e8292b330b8723164f9796
Steeve-Rodrigue <steeve-rodrigue.takougoum@eleve.ensai.fr>
2026-05-12 09:14:33 +0200

    fix: rendering animation bug fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  transpilePackages: [
+    'three',
+    '@react-three/fiber',
+    '@react-three/drei',
+    '@react-three/postprocessing',
+    'postprocessing',
+  ],
 }
 
 export default nextConfig
